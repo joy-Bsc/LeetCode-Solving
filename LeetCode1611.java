67fb2140eec8bf1f50c2fb1b3228b27948156f02
Joy Dhar <dharjoy75@gmail.com>
2025-11-08 23:53:30 +0600

    leet code  solve 8-11-2025  11.50 P.M

---
+++ LeetCode1611.java
@@ -0,0 +1,18 @@
+package problemSolving;
+
+public class LeetCode1611 {
+    public static int minimumOneBitOperations(int n) {
+        int result = 0;
+        while (n > 0) {
+            result ^= n;
+            n >>= 1;
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(minimumOneBitOperations(3)); // Output: 2
+    }
+
+}
