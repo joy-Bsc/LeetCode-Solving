fb89e5f117c790a0f7f3f71fbc5be7ed3529421b
Joy Dhar <dharjoy75@gmail.com>
2025-11-23 01:37:57 +0600

    leet code  solve 23-11-2025  12.30 P.M

---
+++ LeetCode2618.js
@@ -0,0 +1,11 @@
+var checkIfInstanceOf = function(obj, classFunction) {
+    if(obj == null || obj == undefined) return false;
+    if(typeof classFunction !== 'function') return false;
+    let proto = Object.getPrototypeOf(obj);
+    while(proto !== null){
+        if(proto === classFunction.prototype) return true;
+        proto = Object.getPrototypeOf(proto);
+    }
+    return false;
+
+}
